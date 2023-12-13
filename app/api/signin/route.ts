ec9a774f02d1375641b12fa8488fa0c56f44d0a9
Aleksey K <aleksey.karatkevich@mainsoft.org>
2023-12-13 17:35:43 +0300

    initial commit with basiic dbb and signup setup

---
+++ app/api/signin/route.ts
@@ -0,0 +1,66 @@
+import bcrypt from "bcrypt";
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { username, password } = await request.json();
+
+  try {
+    const user = await sql`SELECT * FROM users WHERE username = ${username}`;
+
+    if (user.rows.length > 0) {
+      const passwordMatches = await bcrypt.compare(
+        password,
+        user.rows[0].password
+      );
+
+      if (passwordMatches) {
+        return NextResponse.json({ user }, { status: 200 });
+      } else {
+        return NextResponse.json(
+          { status: "Error", message: "Invalid password" },
+          { status: 403 }
+        );
+      }
+    } else {
+      return NextResponse.json(
+        { status: "Error", message: "User not found" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function GET(request: NextRequest) {
+  const username = request.nextUrl.searchParams.get("username");
+  const password = request.nextUrl.searchParams.get("password");
+
+  try {
+    const user = await sql`SELECT * FROM users WHERE username = ${username}`;
+
+    if (user.rows.length > 0) {
+      const passwordMatches = await bcrypt.compare(
+        password!,
+        user.rows[0].password
+      );
+
+      if (passwordMatches) {
+        return NextResponse.json({ user: user.rows[0] }, { status: 200 });
+      } else {
+        return NextResponse.json(
+          { status: "Error", message: "Invalid password" },
+          { status: 403 }
+        );
+      }
+    } else {
+      return NextResponse.json(
+        { status: "Error", message: "User not found" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
---
+++ app/api/signup/route.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+import { sql } from "@vercel/postgres";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const username = request.nextUrl.searchParams.get("username");
+  const password = request.nextUrl.searchParams.get("password");
+
+  try {
+    const hashedPassword = await bcrypt.hash(password!, 10);
+
+    const result =
+      await sql`INSERT INTO users(username, password) VALUES(${username}, ${hashedPassword})`;
+
+    return NextResponse.json({ result }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
