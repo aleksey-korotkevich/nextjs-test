d0eeba73b139f827d9c169fe4df8e00a7fc642e5
Aleksey K <aleksey.karatkevich@mainsoft.org>
2023-12-14 15:25:24 +0300

    Fixed unescaped character

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -70,7 +70,7 @@ export default function Login() {
 
               {state === "signin" ? (
                 <p className="mt-4 text-sm text-center text-gray-700">
-                  Don't have an account?{" "}
+                  Don&apos;t have an account?{" "}
                   <a
                     onClick={() => setState("signup")}
                     className="font-medium text-blue-600 hover:underline"
